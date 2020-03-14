fe45e8020303d6a6f33d7e30328f8fcd632e5ab6
Guram Bautsadze <guram21@gmail.com>
2020-03-13 20:23:54 -0400

    acorn, minimist Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // constructor () {
+  //   super();
+  //   this.state = { count: 4, counterName: 'First' }
+  // }
+
+  state = {
+    list: [
+      {
+        id: 1, name: 'First todo',
+      },
+      {
+        id: 2, name: 'Second todo',
+      },
+    ],
+    todo: '',
+  }
+
+  componentWillMount () {
+    console.log('componentWillMount')
+  }
+
+  componentWillUnmount () {
+    console.log('componentWillMount')
+    // unplug action
+  }
+
+  componentDidMount () {
+    // fetch().then( this.setState(...) )
+    console.log('componentDidMount')
+  }
+
+  addTodo = () =>
+    // this.setState({ count: this.state.count + 1}) // ok
+
+    this.setState((prev) => ({
+      list: [...prev.list, {
+        id: Math.random(),
+        name: this.state.todo
+      }], todo: ''
+    })) // ok
+
+// this.state.count = 123; // not ok
+  // console.log(this.state)
+
+  inputTodoChange = (e) => {
+    this.setState({ todo: e.target.value})
+  }
+
+  deleteTodo = (id) => {
+    const list = this.state.list.filter(el => el.id !== id)
+    this.setState({ list })
+  }
+
+  render () {
+    console.log('render')
+    return (<div>
+      <input type="text"
+             value={this.state.todo}
+             onChange={this.inputTodoChange}
+             placeholder='Todo name' />
+
+      <button onClick={this.addTodo}>Add to-do</button>
+
+      <ul>
+        {
+          this.state.list.map(el => <li key={el.id}>{el.name}
+          <button onClick={() => this.deleteTodo(el.id)}>Delete</button>
+          </li>)
+        }
+      </ul>
+
+    </div>)
+  }
 }
 
-export default App;
+function App2 () {
+  return (<div>Hello</div>)
+}
+
+export default App
